220dadab0cf4dec42c566d78433c82f7cc87bbb9
Dong Hyeon Lee <jsleeg7279@naver.com>
2022-03-28 11:35:14 +0900

    9205 맥주 마시면서 걸어가기 실버1

---
+++ backjoon/9205번 맥주 마시면서 걸어가기.cpp
@@ -0,0 +1,22 @@
+//Ω√¿€ : 09:09
+//≥° :
+#include <iostream>
+#include <vector>
+
+using namespace std;
+vector<vector<int>> input_map(65536, vector<int>(65536, 0));
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int t;
+	int n;
+	
+
+
+
+	return 0;
+}
