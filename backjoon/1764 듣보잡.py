9ee7c29d5ab0dacce97a780f1c0263debf2efd45
Laptop <jsleeg7279@naver.com>
2023-10-05 19:45:19 +0900

    [BOJ] 1764 듣보잡 실버4

---
+++ backjoon/1764 듣보잡.py
@@ -0,0 +1,26 @@
+import sys
+
+N, M = map(int, input().split())
+
+no_hear = {}
+no_see = {}
+
+for _ in range(N):
+    name = sys.stdin.readline()
+    no_hear[name] = True
+
+for _ in range(M):
+    name = sys.stdin.readline()
+    no_see[name] = True
+
+result = []
+
+for k in no_hear.keys():
+    if not no_see.get(k) == None:
+        result.append(k)
+
+result.sort()
+
+print(len(result))
+for i in result:
+    print(i)
